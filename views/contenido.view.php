9d3feba082906e283fbab5ae57993b7f5088899e
Israel Sanchez <ishisra14@hotmail.com>
2022-01-29 22:06:08 -0600

    Estructura basica 'contenido' y estilos generales

---
+++ views/contenido.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- // ! CSS -->
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <!-- // ! FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&family=Raleway:wght@300;400&family=Roboto:wght@100&display=swap" rel="stylesheet">
+
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar sesion</a>
+
+        <div class="contenido">
+            <article>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aut quia exercitationem tenetur! Magnam similique facilis, voluptates doloribus dignissimos libero odit quis tempora! Sit, libero vitae iste laboriosam odit illo consequuntur explicabo? Ex, eos? Reprehenderit eius numquam quidem iusto doloremque est non. Doloremque quo similique quidem saepe debitis, sit alias eligendi nam, modi veritatis aspernatur incidunt, pariatur beatae. Itaque nemo fugit enim aliquam non, veritatis temporibus debitis, illo dicta consectetur eum ratione! Iste ipsum blanditiis a repellat suscipit eveniet, hic vero ad asperiores rem cupiditate.
+                </p>
+                <br>
+                <p>
+                    Ab consequatur, labore ut autem. Accusamus voluptatum facere aliquam odio doloremque numquam at ullam iure eos culpa? Ut possimus dolorum cumque animi, dicta sequi soluta, porro minima illum modi, eaque ullam iste assumenda! Minus, quisquam. Animi quibusdam accusantium atque sapiente maiores error, est voluptates dolor voluptate commodi nihil omnis veritatis et, ratione, numquam voluptatum! Commodi saepe, dicta officiis quasi distinctio voluptatem itaque exercitationem voluptatum dolorem.
+                </p>
+            </article>
+        </div>
+    </div>
+</body>
+</html>
